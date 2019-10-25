5e78012386846ecd514e1a9f8723e40c49102b46
Petr Miculek <petr.miculek@gmail.com>
2019-10-25 18:34:17 +0200

    Formatting now according to LLDB code style, STATE_ prefix for states some fixes suggested by code analyser in CLion

--- scanner.c
+++ scanner.c
@@ -13,386 +13,441 @@
 unsigned int
 init_string(string_t *string)
 {
-	if ((string->str = (char *) malloc(sizeof(char) * INITIAL_SIZE)) == NULL)
-	{
-		return INTERNAL_ERROR;
-	}
-	string->size = INITIAL_SIZE;
-	string->length = 0;
-	return OK;
+    if ((string->str = (char *) malloc(sizeof(char) * INITIAL_SIZE)) == NULL)
+    {
+        return INTERNAL_ERROR;
+    }
+    string->size = INITIAL_SIZE;
+    string->length = 0;
+    return OK;
 }
 
 unsigned int
 append_string(string_t *string, char var)
 {
-	if (string->length == string->size)
-	{
+    if (string->length == string->size)
+    {
 
-		if ((string->str = (char *) realloc(string->str, sizeof(char) * string->length + REALLOC_SIZE)) == NULL)
-		{
-			return INTERNAL_ERROR;
-		}
-		string->size += REALLOC_SIZE;
-	}
-	strncat(string->str, &var, 1);
-	strncat(string->str, STRING_END, 1);
-	string->length++;
-	//string->str[string->length]= STRING_END;
-	return OK;
+        if ((string->str = (char *) realloc(string->str, sizeof(char) * string->length + REALLOC_SIZE)) == NULL)
+        {
+            return INTERNAL_ERROR;
+        }
+        string->size += REALLOC_SIZE;
+    }
+    strncat(string->str, &var, 1);
+    strncat(string->str, STRING_END, 1);
+    string->length++;
+    //string->str[string->length]= STRING_END;
+    return OK;
 
 }
 
 unsigned int
 free_string(string_t *string)
 {
-	if (string != NULL)
-	{
-		string->size = 0;
-		string->length = 0;
-		free(string->str);
-	}
-	return OK;
+    if (string != NULL)
+    {
+        string->size = 0;
+        string->length = 0;
+        free(string->str);
+    }
+    return OK;
 }
-
 
 unsigned int
 get_token(token_t *token, FILE *file)
 {
-	if (init_string(&token->string))
-	{
-		return INTERNAL_ERROR;
-	}
-	unsigned int state = START;
-	char read;
+    if (init_string(&token->string))
+    {
+        return INTERNAL_ERROR;
+    }
+    unsigned int state = STATE_START;
+    int read;
 
-	while (1)
-	{
+    while (1)
+    {
 
-		read = fgetc(file);
-		switch (state)
-		{
-			case START:
+        read = fgetc(file);
+        if (read == EOF)
+        {
+            // handle
+        }
 
-				if ('1' <= read && read <= '9')
-				{
-					state = INT;
-					APPEND
-					break;
-				} else if (read == '\n')
-				{
-					state = START;
-					break;
-				} else if (read == '\'')
-				{
-					state = LIT;
-					break;
-				} else if (read == ' ')
-				{
-					state = SPACE;
-					break;
-				} else if (read == '"')
-				{
-					state = BLOCK;
-					break;
-				} else if (read == '#')
-				{
-					state = COMMENT;
-					break;
-				} else if ((read >= 'a' && read <= 'z') || (read >= 'A' && read <= 'Z') || (read == '-'))
-				{
+        switch (state)
+        {
+            case STATE_START:
 
-					state = IDENT;
-					APPEND
+                if ('1' <= read && read <= '9')
+                {
+                    state = STATE_INT;
+                    APPEND
+                    break;
+                }
+                else if (read == '\n')
+                {
+                    state = STATE_START;
+                    break;
+                }
+                else if (read == '\'')
+                {
+                    state = STATE_LIT;
+                    break;
+                }
+                else if (read == ' ')
+                {
+                    state = STATE_SPACE;
+                    break;
+                }
+                else if (read == '"')
+                {
+                    state = STATE_BLOCK;
+                    break;
+                }
+                else if (read == '#')
+                {
+                    state = STATE_COMMENT;
+                    break;
+                }
+                else if ((read >= 'a' && read <= 'z') || (read >= 'A' && read <= 'Z') || (read == '-'))
+                {
 
-					break;
-				} else if (read == '!')
-				{
-					state = NEG;
-					break;
-				} else if (read == '0')
-				{
-					RET_ERR;
-				} else if (read == '=')
-				{
-					state = ASSIG;
-					break;
-				} else if (read == '>')
-				{
-					state = MORE;
-					break;
-				} else if (read == '<')
-				{
-					state = LESS;
-					break;
-				} else if (read == '/')
-				{
-					state = DIVISION;
-					token->type = TOKEN_DIVISION;
-					return OK;
-				} else if (read == ':')
-				{
-					state = COLON;
-					token->type = TOKEN_COLON;
-					return OK;
-				} else if (read == '-')
-				{
-					state = MINUS;
-					token->type = TOKEN_MINUS;
-					return OK;
-				} else if (read == '+')
-				{
-					state = PLUS;
-					token->type = TOKEN_PLUS;
-					return OK;
-				} else if (read == '(')
-				{
-					state = LEFT;
-					token->type = TOKEN_LEFT;
-					return OK;
-				} else if (read == ')')
-				{
-					state = RIGHT;
-					token->type = TOKEN_RIGHT;
-					return OK;
-				} else if (read == EOF)
-				{
-					state = END;
-					token->type = TOKEN_END;
-					return OK;
-				} else if (read == '*')
-				{
-					state = MULTI;
-					token->type = TOKEN_MULTI;
-					return OK;
-				} else if (read == ',')
-				{
-					state = COMMA;
-					token->type = TOKEN_COMMA;
-					return OK;
-				} else
-				{
-					state = ERROR;
-					RET_ERR
-				}
+                    state = STATE_INDENT;
+                    APPEND
 
-			case INT:
-				if (read == '.')
-				{
-					state = FLOAT;
-					APPEND
-					break;
-				} else if ('0' <= read && read <= '9')
-				{
-					state = INT;
-					APPEND
-					break;
-				} else if (read == 'e' || read == 'E')
-				{
-					state = FLOAT_E;
-					APPEND
-					break;
-				} else if ((39 < read && read < 48) || (59 < read && read < 63) || read == ' ' || read == '"' ||
-						   read == '\n')
-				{ //   <> () *+=-/ " "  # " ,
+                    break;
+                }
+                else if (read == '!')
+                {
+                    state = STATE_NEG;
+                    break;
+                }
+                else if (read == '0')
+                {
+                    RET_ERR;
+                }
+                else if (read == '=')
+                {
+                    state = STATE_ASSIGN;
+                    break;
+                }
+                else if (read == '>')
+                {
+                    state = STATE_MORE;
+                    break;
+                }
+                else if (read == '<')
+                {
+                    state = STATE_LESS;
+                    break;
+                }
+                else if (read == '/')
+                {
+                    state = STATE_DIVISION;
+                    token->type = TOKEN_DIVISION;
+                    return OK;
+                }
+                else if (read == ':')
+                {
+                    state = STATE_COLON;
+                    token->type = TOKEN_COLON;
+                    return OK;
+                }
+                else if (read == '-')
+                {
+                    /* FIXME    this condition can never be true
+                                because there is '-' included in
+                                STATE_INDENT case
+                     */
+                    state = STATE_MINUS;
+                    token->type = TOKEN_MINUS;
+                    return OK;
+                }
+                else if (read == '+')
+                {
+                    state = STATE_PLUS;
+                    token->type = TOKEN_PLUS;
+                    return OK;
+                }
+                else if (read == '(')
+                {
+                    state = STATE_LEFT;
+                    token->type = TOKEN_LEFT;
+                    return OK;
+                }
+                else if (read == ')')
+                {
+                    state = STATE_RIGHT;
+                    token->type = TOKEN_RIGHT;
+                    return OK;
+                }
+                else if (read == EOF)
+                {
+                    state = STATE_END;
+                    token->type = TOKEN_END;
+                    return OK;
+                }
+                else if (read == '*')
+                {
+                    state = STATE_MULTI;
+                    token->type = TOKEN_MULTI;
+                    return OK;
+                }
+                else if (read == ',')
+                {
+                    state = STATE_COMMA;
+                    token->type = TOKEN_COMMA;
+                    return OK;
+                }
+                else
+                {
+                    state = STATE_ERROR;
+                    RET_ERR
+                }
 
-					token->type = TOKEN_INT;
-					ungetc(read, file);
+            case STATE_INT:
+                if (read == '.')
+                {
+                    state = STATE_FLOAT;
+                    APPEND
+                    break;
+                }
+                else if ('0' <= read && read <= '9')
+                {
+                    state = STATE_INT;
+                    APPEND
+                    break;
+                }
+                else if (read == 'e' || read == 'E')
+                {
+                    state = STATE_FLOAT_E;
+                    APPEND
+                    break;
+                }
+                else if ((39 < read && read < 48) || (59 < read && read < 63) || read == ' ' || read == '"' ||
+                    read == '\n')
+                { //   <> () *+=-/ " "  # " ,
 
-					return OK;
-				} else
-				{
-					state = ERROR;
-					RET_ERR
-				}
-			case FLOAT:
-				if ('0' <= read && read <= '9')
-				{
-					state = FLOAT_D;
-					APPEND
-					break;
-				} else
-				{
-					state = ERROR;
-					RET_ERR
-				}
-			case FLOAT_D:
-				if ('0' <= read && read <= '9')
-				{
-					state = FLOAT_D;
-					APPEND
-					break;
-				} else if (read == 'e' || read == 'E')
-				{
-					state = FLOAT_E;
-					APPEND
-					break;
-				} else if ((39 < read && read < 48) || (59 < read && read < 63) || read == ' ' || read == '"' ||
-						   read == '\n')
-				{ //   <> () *+=-/ " "  # " ,
-					token->type = TOKEN_FLOAT;
-					ungetc(read, file);
-					return OK;
-				} else
-				{
-					state = ERROR;
-					RET_ERR
-				}
-			case FLOAT_E:
-				if ('1' <= read && read <= '9')
-				{
-					state = FLOAT_N;
-					APPEND
-					break;
-				} else if (read == '0')
-				{
-					state = FLOAT_E;
-					break;
-				} else if (read == '+' || read == '-')
-				{
-					state = FLOAT_S;
-					APPEND
-					break;
-				} else
-				RET_ERR
-			case FLOAT_N:
-				if ('0' <= read && read <= '9')
-				{
-					state = FLOAT_N;
-					APPEND
-					break;
-				} else if ((39 < read && read < 48) || (59 < read && read < 63) || read == ' ' || read == '"' ||
-						   read == '\n')
-				{
-					token->type = TOKEN_FLOAT;
-					ungetc(read, file);
-					return OK;
-				} else
-				RET_ERR
+                    token->type = TOKEN_INT;
+                    ungetc(read, file);
 
-			case FLOAT_S:
-				if ('0' <= read && read <= '9')
-				{
-					state = FLOAT_N;
-					APPEND
-					break;
-				} else if ((39 < read && read < 48) || (59 < read && read < 63) || read == ' ' || read == '"' ||
-						   read == '\n')
-				{
-					token->type = TOKEN_FLOAT;
-					ungetc(read, file);
-					return OK;
-				} else
-				RET_ERR
+                    return OK;
+                }
+                else
+                {
+                    state = STATE_ERROR;
+                    RET_ERR
+                }
+            case STATE_FLOAT:
+                if ('0' <= read && read <= '9')
+                {
+                    state = STATE_FLOAT_D;
+                    APPEND
+                    break;
+                }
+                else
+                {
+                    state = STATE_ERROR;
+                    RET_ERR
+                }
+            case STATE_FLOAT_D:
+                if ('0' <= read && read <= '9')
+                {
+                    state = STATE_FLOAT_D;
+                    APPEND
+                    break;
+                }
+                else if (read == 'e' || read == 'E')
+                {
+                    state = STATE_FLOAT_E;
+                    APPEND
+                    break;
+                }
+                else if ((39 < read && read < 48) || (59 < read && read < 63) || read == ' ' || read == '"' ||
+                    read == '\n')
+                { //   <> () *+=-/ " "  # " ,
+                    token->type = TOKEN_FLOAT;
+                    ungetc(read, file);
+                    return OK;
+                }
+                else
+                {
+                    state = STATE_ERROR;
+                    RET_ERR
+                }
+            case STATE_FLOAT_E:
+                if ('1' <= read && read <= '9')
+                {
+                    state = STATE_FLOAT_N;
+                    APPEND
+                    break;
+                }
+                else if (read == '0')
+                {
+                    state = STATE_FLOAT_E;
+                    break;
+                }
+                else if (read == '+' || read == '-')
+                {
+                    state = STATE_FLOAT_S;
+                    APPEND
+                    break;
+                }
+                else
+                    RET_ERR
+            case STATE_FLOAT_N:
+                if ('0' <= read && read <= '9')
+                {
+                    state = STATE_FLOAT_N;
+                    APPEND
+                    break;
+                }
+                else if ((39 < read && read < 48) || (59 < read && read < 63) || read == ' ' || read == '"' ||
+                    read == '\n')
+                {
+                    token->type = TOKEN_FLOAT;
+                    ungetc(read, file);
+                    return OK;
+                }
+                else
+                    RET_ERR
 
-			case LIT:
-				if (read == '\\')
-				{
-					state = LIT_B;
-					APPEND
-					break;
-				} else if (read == '\'')
-				{
-					token->type = TOKEN_LIT;
-					return OK;
-				} else if (31 < read)
-				{
-					state = LIT;
-					APPEND
-					break;
-				} else
-				RET_ERR
-			case LIT_B:
-				if (read == 'x')
-				{
-					state = LIT_H;
-					APPEND
-					break;
-				} else
-				{
-					state = LIT;
-					APPEND
-					break;
-				}
-			case LIT_H:
-				if (('0' <= read && read <= '9') || ('a' <= read && read <= 'f') || ('A' <= read && read <= 'F'))
-				{
-					state = LIT_H1;
-					APPEND
-					break;
-				} else
-				RET_ERR
-			case LIT_H1:
-				if (('0' <= read && read <= '9') || ('a' <= read && read <= 'f') || ('A' <= read && read <= 'F'))
-				{
-					state = LIT;
-					APPEND
-					break;
-				} else
-				RET_ERR
-			case BLOCK:
-				if (read == '"')
-				{
-					state = BLOCK1;
-					break;
-				} else
-				RET_ERR
-			case BLOCK1:
-				if (read == '"')
-				{
-					state = BLOCK2;
-					break;
-				} else
-				RET_ERR
-			case BLOCK2:
-				if (read == '"')
-				{
-					state = BLOCK_B;
-					break;
-				} else if (31 < read)
-				{
-					state = BLOCK2;
-				} else
-				RET_ERR
-			case BLOCK_B:
-				if (read == '"')
-				{
-					state = BLOCK1;
-					break;
-				} else
-				RET_ERR
-			case BLOCK_B1:
-				if (read == '"')
-				{
-					state = START;
-					break;
-				} else
-				RET_ERR
-			case COMMENT:
-				if (read == '\n')
-				{
-					state = START;
-					break;
-				} else
-				{
-					state = COMMENT;
-					break;
-				}
-			case IDENT:
-				if ((read >= 'a' && read <= 'z') || (read >= 'A' && read <= 'Z') || (read == '-') ||
-					(read >= '0' && read <= '9'))
-				{
-					state = IDENT;
-					APPEND
-					break;
-				} else
-				RET_ERR
+            case STATE_FLOAT_S:
+                if ('0' <= read && read <= '9')
+                {
+                    state = STATE_FLOAT_N;
+                    APPEND
+                    break;
+                }
+                else if ((39 < read && read < 48) || (59 < read && read < 63) || read == ' ' || read == '"' ||
+                    read == '\n')
+                {
+                    token->type = TOKEN_FLOAT;
+                    ungetc(read, file);
+                    return OK;
+                }
+                else
+                    RET_ERR
 
+            case STATE_LIT:
+                if (read == '\\')
+                {
+                    state = STATE_LIT_B;
+                    APPEND
+                    break;
+                }
+                else if (read == '\'')
+                {
+                    token->type = TOKEN_LIT;
+                    return OK;
+                }
+                else if (31 < read)
+                {
+                    state = STATE_LIT;
+                    APPEND
+                    break;
+                }
+                else
+                    RET_ERR
+            case STATE_LIT_B:
+                if (read == 'x')
+                {
+                    state = STATE_LIT_H;
+                    APPEND
+                    break;
+                }
+                else
+                {
+                    state = STATE_LIT;
+                    APPEND
+                    break;
+                }
+            case STATE_LIT_H:
+                if (('0' <= read && read <= '9') || ('a' <= read && read <= 'f') || ('A' <= read && read <= 'F'))
+                {
+                    state = STATE_LIT_H1;
+                    APPEND
+                    break;
+                }
+                else
+                    RET_ERR
+            case STATE_LIT_H1:
+                if (('0' <= read && read <= '9') || ('a' <= read && read <= 'f') || ('A' <= read && read <= 'F'))
+                {
+                    state = STATE_LIT;
+                    APPEND
+                    break;
+                }
+                else
+                    RET_ERR
+            case STATE_BLOCK:
+                if (read == '"')
+                {
+                    state = STATE_BLOCK1;
+                    break;
+                }
+                else
+                    RET_ERR
+            case STATE_BLOCK1:
+                if (read == '"')
+                {
+                    state = STATE_BLOCK2;
+                    break;
+                }
+                else
+                    RET_ERR
+            case STATE_BLOCK2:
+                if (read == '"')
+                {
+                    state = STATE_BLOCK_B;
+                    break;
+                }
+                else if (31 < read)
+                {
+                    state = STATE_BLOCK2;
+                }
+                else
+                    RET_ERR
+            case STATE_BLOCK_B:
+                if (read == '"')
+                {
+                    state = STATE_BLOCK1;
+                    break;
+                }
+                else
+                    RET_ERR
+            case STATE_BLOCK_B1:
+                if (read == '"')
+                {
+                    state = STATE_START;
+                    break;
+                }
+                else
+                    RET_ERR
+            case STATE_COMMENT:
+                if (read == '\n')
+                {
+                    state = STATE_START;
+                    break;
+                }
+                else
+                {
+                    state = STATE_COMMENT;
+                    break;
+                }
+            case STATE_INDENT:
+                if ((read >= 'a' && read <= 'z') || (read >= 'A' && read <= 'Z') || (read == '-') ||
+                    (read >= '0' && read <= '9'))
+                {
+                    state = STATE_INDENT;
+                    APPEND
+                    break;
+                }
+                else
+                    RET_ERR
 
-			default:
-				state = ERROR;
-				break;
-		}
-	}
-
+            default: state = STATE_ERROR;
+                break;
+        }
+    }
 
 }
