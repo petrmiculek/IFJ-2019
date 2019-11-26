308cf46c286e5a20f449a7a04ae964dab40ae0a5
Daniel Jacko <daniel.jacko14@gmail.com>
2019-11-26 12:45:02 +0100

    handle first line, no dedent on epmty line

--- src/scanner.c
+++ src/scanner.c
@@ -140,6 +140,7 @@ get_token(token_t *token, FILE *file)
     unsigned int state = STATE_START;
     int read, flag_block_es = 0;
     char var[2];
+    static int first_call = 1;
 
     int check_dedent = generate_dedent(&spaces_num, token, &previous_was_eol);
     if (check_dedent == 1)
@@ -163,6 +164,42 @@ get_token(token_t *token, FILE *file)
         switch (state)
         {
             case STATE_START:
+
+                if (first_call == 2)
+                {
+                    first_call = 0 ;
+                    if(read == ' ')
+                    {
+                        first_call = 2;
+                    }
+                    else if (read == '#')
+                    {
+                        state = STATE_COMMENT;
+                    }
+                    else if (read == '"')
+                    {
+                        state = STATE_BLOCK1;
+                    }
+                    else if (read == EOF)
+                    {
+                        token->type = TOKEN_EOF;
+                        return RET_OK;
+                    }
+                    else
+                    {
+                        RETURN_ERR
+                    }
+                    break;
+                }
+                if (first_call == 1)
+                {
+                    first_call = 0;
+                    if (read == ' ')
+                    {
+                        first_call = 2;
+                        break;
+                    }
+                }
 
                 if ('0' <= read && read <= '9')
                 {
@@ -298,6 +335,10 @@ get_token(token_t *token, FILE *file)
                 }
                 else
                 {
+                    if(read<33)
+                    {
+                        spaces_num = -1;
+                    }
                     ungetc(read, file);
                     token->type = TOKEN_EOL;
                     previous_was_eol = 2;
@@ -320,6 +361,9 @@ get_token(token_t *token, FILE *file)
                 }
                 else
                 {
+                    if(read<33){
+                        spaces_num = -1;
+                    }
                     ungetc(read, file);
                     token->type = TOKEN_EOL;
                     previous_was_eol = 2;
