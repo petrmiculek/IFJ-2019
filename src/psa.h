82a970f6a0e29b69236fb395ff565263776a1519
Petr Miculek <petr.miculek@gmail.com>
2019-11-27 13:15:18 +0100

    Revert "spaghetti fix for psa build problems"
    This reverts commit 38215180

--- src/psa.h
+++ src/psa.h
@@ -9,19 +9,54 @@
  */
 
 #include "scanner.h"
-#include "exp_stack.h"
-#include "psa_stack_declarations.h"
 
 unsigned int
 get_symbol(token_t *token);
 
-unsigned int
-check_semantics(rules rule, sem_t *sym1, sem_t *sym2, sem_t *sym3, d_type *final_type);
+unsigned int 
+check_semantics(rules rule, sem_t *sym1, sem_t *sym2, sem_t *sym3, d_type* final_type);
 
 #define EXP 42
 #define SHIFT 21
 
+typedef enum
+{
+    PLUS,
+    MIN,
+    MUL,
+    DIV,
+    IDIV,
+    A,
+    EA,
+    L,
+    EL,
+    EQ,
+    NE,
+    OP_INT,
+    OP_FLOAT,
+    OP_STR,
+    OP_ID,
+    DOLAR,
+    L_BRAC,
+    R_BRAC
+}table_symbol;
 
+typedef enum rules
+{
+    R_I,
+    R_PLUS,
+    R_MIN,
+    R_MUL,
+    R_DIV,
+    R_IDIV,
+    R_A,
+    R_EA,
+    R_L,
+    R_EL,
+    R_EQ,
+    R_NE,
+    R_BRACKETS
+} rules;
 
 unsigned int tmp_var();
 #endif // HEADER_PSA
